cfe22742cf0ef2cc405921be0ccfe6fa5706ffe9
Leoooo22 <leonardo.lorenzon1202@gmail.com>
2025-09-14 14:01:51 +0200

    add: admin page and routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,8 +3,11 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { SearchFlightsComponent } from './components/flights/search-flights/search-flights.component';
 import { AirlinesHomeComponent } from './components/airlines/airlines-home/airlines-home.component';
+import { AirlinesDashboardComponent } from './components/airlines/airlines-home/airlines-dashboard/airlines-dashboard.component';
 import { AircraftsComponent } from './components/airlines/aircrafts/aircrafts.component';
 import { RoutesComponent } from './components/airlines/routes/routes.component';
+import { AdminHomeComponent } from './components/admin/admin-home/admin-home.component';
+// import { FlightsComponent } from './components/airlines/flights/flights.component';
 
 
 export const routes: Routes = [
@@ -21,11 +24,17 @@ export const routes: Routes = [
         component: RegisterComponent
     },
     {
+        path: 'admin',
+        component: AdminHomeComponent
+    },
+    {
         path: 'airlines',
         component: AirlinesHomeComponent,
         children: [
+            { path: '', pathMatch: 'full', component: AirlinesDashboardComponent },
             { path: 'aircrafts', component: AircraftsComponent },
-            { path: 'routes', component : RoutesComponent}
+            { path: 'routes', component : RoutesComponent }
+            // { path: 'flights', component: FlightsComponent }
         ]
     }
     // {
